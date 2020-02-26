9c302550d33c99252de7812926948309b2185590
tendiarifin <tendiarifin0308@gmail.com>
2020-02-26 07:32:07 +0700

    buat Drawable

---
+++ app/src/main/java/com/example/latihandicodingtendi/Item.kt
@@ -0,0 +1,9 @@
+package com.example.latihandicodingtendi
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.ViewParent
+
+data class Item (val name: String?,val image : Int?)
+
